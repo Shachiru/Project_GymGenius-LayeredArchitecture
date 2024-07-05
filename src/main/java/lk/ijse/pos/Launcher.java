ca86ca549d29a8917f52131cf74e850a194cc0be
shach <shachirurashmika35@gmail.com>
2024-07-05 12:10:23 +0530

    dashboard

--- src/main/java/lk/ijse/pos/Launcher.java
+++ src/main/java/lk/ijse/pos/Launcher.java
@@ -14,7 +14,7 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/member_form.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"))));
         stage.setTitle("Gym Genius");
         stage.centerOnScreen();
         stage.setResizable(false);
