7d748d896350a90777ac9600093b4ce916716141
shach <shachirurashmika35@gmail.com>
2024-07-05 12:27:04 +0530

    dashboard loaded

--- src/main/java/lk/ijse/pos/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/pos/controller/DashboardFormController.java
@@ -1,13 +1,22 @@
 package lk.ijse.pos.controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import lk.ijse.pos.util.Navigation;
 
-public class DashboardFormController {
+import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class DashboardFormController implements Initializable {
 
     @FXML
     private JFXButton btnEmp;
@@ -31,33 +40,74 @@ public class DashboardFormController {
     private AnchorPane rootNode;
 
     @FXML
-    void btnEmpOnAction(ActionEvent event) {
-
+    void btnHomeOnAction(ActionEvent event) throws IOException {
+        Navigation.switchPaging(pagingPane,"dashboardDetail_form.fxml ");
     }
 
     @FXML
-    void btnHomeOnAction(ActionEvent event) {
-
+    void btnEmpOnAction(ActionEvent event) throws IOException {
+        Navigation.switchPaging(pagingPane,"employee_form.fxml");
     }
 
     @FXML
     void btnLogoutOnAction(ActionEvent event) {
-
+        btnLogout.getScene().getWindow().hide();
+        Navigation.changeStage("/view/login_form.fxml","Login Form");
     }
 
     @FXML
-    void btnMembersOnAction(ActionEvent event) {
-
+    void btnMembersOnAction(ActionEvent event) throws IOException {
+        Navigation.switchPaging(pagingPane,"member_form.fxml");
     }
 
     @FXML
-    void btnShopOnAction(ActionEvent event) {
-
+    void btnShopOnAction(ActionEvent event) throws IOException {
+        Navigation.switchPaging(pagingPane,"order_form.fxml");
     }
 
     @FXML
-    void btnSupplementOnAction(ActionEvent event) {
+    void btnSupplementOnAction(ActionEvent event) throws IOException {
+        Navigation.switchPaging(pagingPane,"supplement_form.fxml");
+    }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        timeNow();
+        pagingPane.setVisible(true);
+        try {
+            Navigation.switchPaging(pagingPane,"dashboardDetail_form.fxml ");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private volatile boolean running = true;
+
+    private void timeNow() {
+        Thread thread = new Thread(() -> {
+            SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss a");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+            while (running) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+                final String currentTime = timeFormat.format(new Date());
+                final String currentDate = dateFormat.format(new Date());
+                Platform.runLater(() -> {
+                    lblTime.setText(currentTime);
+                    lblDate.setText(currentDate);
+                });
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    public void stopTimeThread() {
+        running = false;
     }
 
 }
